8c75e6fe6ab0172911f9e21de5ff6ffb42154eed
collinskoech11 <collinskoechck34@gmail.com>
2021-10-12 13:42:01 +0300

    axios fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import * as React from "react";
 import './App.css';
+import axios from "axios";
 
 const {useState} = React;
+
+// https://randomuser.me/api
 
 function App() {
   const [counter, setCounter] = useState(0)
@@ -12,9 +15,25 @@ function App() {
       <p>{counter}</p>
       <button onClick={() => {
         setCounter(counter + 1);
+        console.log('foo')
       }}>Increase Counter</button>
+      <button onClick={() => {
+        fetchRandomData();
+      }}>Fetch Random data</button>
     </div>
   );
 }
+const fetchRandomData = () => {
+  return axios.get('https://randomuser.me/api')
+  .then(res => {
+    //handle access
+    console.log(res);
+    return res;
+  })
+  .catch(err => {
+    //handle error
+    console.error(err);
+  });
 
+}
 export default App;
